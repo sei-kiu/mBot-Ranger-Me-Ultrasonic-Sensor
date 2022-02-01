5623e673e57093b3e32a6be8eb16262ef6ef3a3a
sei-kiu <sei.kiu.sei.kiu@gmail.com>
2022-02-02 02:48:43 +0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,7 @@
 #include <Arduino.h>
+#include <MeAuriga.h>
 
-/**
- * \par Copyright (C), 2012-2016, MakeBlock
- * @file    UltrasonicSensorTest.ino
- * @author  MakeBlock
- * @version V1.0.0
- * @date    2015/09/01
- * @brief   Description: this file is sample code for Me ultrasonic sensor module.
- *
- * Function List:
- * 1. double MeUltrasonicSensor::distanceCm(uint16_t MAXcm)
- *
- * \par History:
- * <pre>
- * <Author>     <Time>        <Version>      <Descr>
- * Mark Yan     2015/09/01    1.0.0          rebuild the old lib
- * </pre>
- */
-#include "MeAuriga.h"
-
-MeUltrasonicSensor ultraSensor(PORT_7); /* Ultrasonic module can ONLY be connected to port 3, 4, 6, 7, 8 of base shield. */
+MeUltrasonicSensor ultrasonic_10(10);
 
 void setup()
 {
@@ -29,7 +11,7 @@ void setup()
 void loop()
 {
   Serial.print("Distance : ");
-  Serial.print(ultraSensor.distanceCm() );
+  Serial.print(ultrasonic_10.distanceCm() );
   Serial.println(" cm");
-  delay(100); /* the minimal measure interval is 100 milliseconds */
+  delay(100);
 }
